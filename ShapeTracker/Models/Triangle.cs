376013ec51cbb6b82a1a210d269e93a71759dc4d
Brooke Kullberg <brookekullberg@gmail.com>
2023-02-02 15:07:04 -0800

    add working project

---
+++ ShapeTracker/Models/Triangle.cs
@@ -0,0 +1,7 @@
+namespace ShapeTracker.Models
+{
+  class Triangle
+  {
+    // Code for Triangle business logic will go here.
+  }
+}
---
+++ ShapeTracker/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using ShapeTracker.Models;
+
+namespace ShapeTracker
+{
+  class Program
+  {
+    static void Main()
+    {
+      Triangle testTriangle = new Triangle();
+      Console.WriteLine(testTriangle);
+    }
+  }
+}
